c366a20e65c8211ae5c34b1e134b31034f1b23e0
Bryan <bubryanwb@gmail.com>
2026-04-25 02:47:55 +0200

    refactor: 🚧 Terraformed

--- assets/js/cv-jetbot.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('scrollToTop').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('#endresult').scrollIntoView({ behavior: 'smooth' });
-});
